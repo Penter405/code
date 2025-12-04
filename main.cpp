971eba8467121e74f6127a0b6efcb83637f1b25a
Penter405 <bsid405@gmail.com>
2025-12-04 14:40:55 +0000

    update

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter a integer:";cin>>x;
+    cout<<"your integer is :"<<x<<"\n";
+    cout<<"done";
+    return 0;
+}
