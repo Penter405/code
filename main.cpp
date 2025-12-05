a5e223eb52e639b296f6f99173180c62d5aaf957
Penter405 <bsid405@gmail.com>
2025-12-05 04:10:31 +0000

    update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
+class human{
+    int hight,kg;
+    char name[10];
+    void output_name(){
+       cout<<name; 
+    }
+};
+
 int main(){
     int x;
     cout<<"enter a integer:";cin>>x;
     cout<<"your integer is :"<<x<<"\n";
     cout<<"done";
+
     return 0;
+}
+int math(){
+
+
+}
+int pawn(){
+    
 }
