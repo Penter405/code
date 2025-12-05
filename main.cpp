2d2eb83d7d533c65cd129b9d25082536842a1324
Penter405 <bsid405@gmail.com>
2025-12-05 14:52:18 +0000

    update

--- main.cpp
+++ main.cpp
@@ -5,28 +5,29 @@ class human{
     int hight,kg;
     char name[11];
     void output_name(){
-       cout<<name;
+       cout<<name<<"\n";
     }
 };
 
 int math(){
     human pencel;
     pencel.kg=99;
-    cout<<pencel.kg;
-    cout<<"enter your name(under 11 character)";cin>>pencel.name;
+    cout<<pencel.kg<<"\n";
+    cout<<"enter your name(under 11 character):";cin>>pencel.name;
     pencel.output_name();
     return 0;
 }
 
-int pawn(){
+int first_try(){
+    int x;
+    cout<<"enter a integer:";cin>>x;
+    cout<<"your integer is :"<<x<<"\n";
+    cout<<"done\n";
     return 0;
 }
 
 int main(){
-    int x;
-    cout<<"enter a integer:";cin>>x;
-    cout<<"your integer is :"<<x<<"\n";
-    cout<<"done";
+    first_try();
     math();
     return 0;
 }
