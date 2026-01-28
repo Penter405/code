04953df3a0d78264e29f2a5dd05f529753382527
Penter405 <bsid405@gmail.com>
2026-01-28 10:11:48 +0000

    leetcode 121

---
+++ leetcode/121.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int before_lowest=prices[0],profit=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]-before_lowest>profit){
+                profit=prices[i]-before_lowest;
+            }
+            if(prices[i]<before_lowest){
+                before_lowest=prices[i];
+            }
+        }
+        return profit;
+    }
+};
