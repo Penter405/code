04b1fcd8217e0f1447e715e0feb13382cd136674
Penter405 <bsid405@gmail.com>
2026-02-05 11:45:58 +0000

    leetcode 11

--- leetcode/11.cpp
+++ leetcode/11.cpp
@@ -1,1 +1,34 @@
 //it is some version in python ide
+#include <vector>
+#include <iostream>
+class Solution {
+private:
+    int min(int a,int b){
+        if(a<b){
+            return a;
+        }
+        return b;
+    }
+public:
+    int maxArea(std::vector<int>& height) {
+        auto front=height.begin();
+        auto back=height.rbegin();
+        int big=-1;
+        int len=height.size()-1;
+        for(;len!=0;){
+            
+            if(len*min(*front,*back)>big){
+                big=len*min(*front,*back);
+            }
+            //std::cout<<len<<" * "<<min(*front,*back)<<" == "<<big<<'\n';
+            if(*front<*back){
+                front++;
+            }else{
+                back++;
+            }
+
+            len--;
+        }
+        return big;
+    }
+};
