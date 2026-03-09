3aed2d803369b01e64cab4786b87d16bdacb331e
Penter405 <bsid405@gmail.com>
2026-03-09 12:48:35 +0000

    leetcode 204

---
+++ leetcode/204.cpp
@@ -0,0 +1,28 @@
+#include <iostream
+using namespace std;
+class Solution {
+public:
+    int countPrimes(int n) {
+        int result=0;
+        std::vector<int> is_prime(n,1);
+        /*for(auto&it:is_prime){
+            std::cout<<it<<"\n";
+        }
+        */
+        if(n==0||n==1){
+            return 0;
+        }
+        is_prime[0]=0;//we dont use 0 element
+        is_prime[1]=0;//int 1 wont be prime
+        for(int i=1;i<n;++i){
+            if(is_prime[i]){
+                //std::cout<<"int "<<i<<"is prime\n";
+                ++result;
+                for(int pe=i+i;pe<n;pe+=i){
+                    is_prime[pe]=0;
+                }
+            }
+        }
+        return result;
+    }
+};
