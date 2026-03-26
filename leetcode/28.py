e93daab67f5838847318d7a7776c2fa08ccb4ea7
Penter405 <bsid405@gmail.com>
2026-03-26 11:05:55 +0000

    leetcode 28, python and string to int.py

--- leetcode/28.py
+++ leetcode/28.py
@@ -1,26 +1,8 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        p2=0
-        meet=-1
-        now=0
-        while p2<len(haystack):
-            print(p2,now)
-            
-            if haystack[p2]==needle[now]:
-                if 
-                if now>=len(needle)-1 and meet!=-1:
-                    print(p2,now)
-                    return meet
-                if now==0:
-                    meet=p2
-                now+=1
-                p2+=1
-
-                
-            else:
-                p2+=1
-                now=0
-                meet=-1
-        if meet!=-1:
-            return meet
+        rs=len(haystack)-len(needle)+1
+        for i in range(rs):
+            print(haystack[i:i+len(needle)],needle)
+            if haystack[i:i+len(needle)]==needle:
+                return i
         return -1
