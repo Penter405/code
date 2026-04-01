c0a2266df793a2cf9bec35999a5a8b1dafe71c5c
Penter405 <bsid405@gmail.com>
2026-04-01 14:56:55 +0000

    java IO trying, now dont know default date type of input output

---
+++ Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        String name1=System.in.readLine();
+        String name2=System.in.readLine();
+        System.out.println(name1);
+        System.out.printIn(name2);
+    }
+}
