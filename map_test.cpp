3d450799a9d3e0e03b0c45f88623846f9e34d8cb
Penter405 <bsid405@gmail.com>
2026-04-02 11:30:07 +0000

    map test

--- map_test.cpp
+++ map_test.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 int main(){
-    std::map<std::string, std::string> hash;
-    hash["whats_up"]="im penter";
-    std::cout<<hash["whats_up"]<<std::endl;
+    std::vector<int> v={1,2,3};
+    std::map<std::vector<int>, std::string> hash;
+    std::map<std::string, std::string> hash2;
+    hash[v]="im penter";
+    hash2["whats_up"]="im penter";
+    std::cout<<hash[v]<<std::endl;
+    std::cout<<hash2["whats_up"]<<std::endl;
 }
