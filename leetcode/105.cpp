3fc418b7dd91b559d54499ce6186a52f3921503f
Penter405 <bsid405@gmail.com>
2026-04-18 08:02:38 +0000

    leetcode 105

--- leetcode/105.cpp
+++ leetcode/105.cpp
@@ -37,10 +37,11 @@ public:
         bst_to_object_pointer[value_to_bst_index[preorder[0]]]=(p);
         for(int i=1;i<preorder.size();++i){
             if(value_to_bst_index[preorder[i]]<bst_index_stack[index_stack]){
-                std::cout<<value_to_bst_index[preorder[i]]<<" < "<<bst_index_stack[index_stack]<<"\n";
+                //std::cout<<value_to_bst_index[preorder[i]]<<" < "<<bst_index_stack[index_stack]<<"\n";
                 TreeNode* p_child;
                 p_child=new TreeNode(preorder[i]);
                 (*bst_to_object_pointer[bst_index_stack[index_stack]]).left=(p_child);
+                //std::cout<<(*bst_to_object_pointer[bst_index_stack[index_stack]]).val<<" his left is "<<(*p_child).val<<"\n";
                 bst_index_stack.push_back(value_to_bst_index[preorder[i]]);
                 ++index_stack;
                 bst_to_object_pointer[value_to_bst_index[preorder[i]]]=(p_child);
@@ -48,9 +49,10 @@ public:
             }else{
                 //now seeing must bigger than root, so we cheak which small
                 //but not left could be someone's right
-                std::cout<<value_to_bst_index[preorder[i]]<<" > "<<bst_index_stack[index_stack]<<"\n";
+                //std::cout<<value_to_bst_index[preorder[i]]<<" > "<<bst_index_stack[index_stack]<<"\n";
                 while(index_stack!=0 && value_to_bst_index[preorder[i]] >bst_index_stack[index_stack-1]){
                     //bst_index_stack smallest is 1, and 1-1=0, no address fetch wrong
+                    //std::cout<<"we are seeing "<<value_to_bst_index[preorder[i]]<<" it more than "<<bst_index_stack[index_stack-1]<<"  so we should clear stack when it fetch to right?, if the node go to right, him self remove.\n";
                     bst_index_stack.pop_back();
                     --index_stack;
                 }
@@ -58,8 +60,9 @@ public:
                 TreeNode* p_child;
                 p_child=new TreeNode(preorder[i]);
                 (*bst_to_object_pointer[bst_index_stack[index_stack]]).right=(p_child);
-                bst_index_stack.push_back(value_to_bst_index[preorder[i]]);
-                ++index_stack;
+                //std::cout<<(*bst_to_object_pointer[bst_index_stack[index_stack]]).val<<" his right is "<<(*p_child).val<<"\n";
+                bst_index_stack[index_stack]=(value_to_bst_index[preorder[i]]);//origin is push back, but now we remove it self, because as one node's left and right has child, no more node can be his child again
+                //++index_stack;
                 //std::cout<<"hi";
                 bst_to_object_pointer[value_to_bst_index[preorder[i]]]=(p_child);
             }
