62a4aed422126f89a9c21673082beba2b1b85ab5
Penter405 <bsid405@gmail.com>
2026-04-22 10:08:48 +0000

    leetcode 987 python

--- leetcode/987_help.py
+++ leetcode/987_help.py
@@ -1,6 +1,21 @@
-s=dict()
-while True:
-    s=input()
-    if s=="0":
-        break
-for rs i
+def f_1():
+    s=dict()
+    while True:
+        r=input()
+        if r=="0":
+            break
+        a=input()
+        s[r]=a
+
+
+    for rs in sorted(s.keys()):
+        print(rs,s[rs])
+def f_2():
+    help(list)
+def f_3():
+    a=[]
+    print(a==None)
+def f_4():
+    s=[]
+    s.extend(sorted([3]))
+f_4()
