65ac2658113c1c6d4fa474da564c3f8222717b3c
Penter405 <bsid405@gmail.com>
2026-07-27 23:56:16 +0000

    leetcode 402.py 1464.py

--- leetcode/6.py
+++ leetcode/6.py
@@ -1,20 +1,34 @@
 #0:10:43 unfinished
+#0:10:49 unfinished
+#the shape is reversed N of juat h
 #the shape is reversed N of juat h
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         #3 1 3 1 to get first eleent
+        if numRows==1:
+            return s
         a=[]
         b=[]
-        ever=-1
-        for rs in numRows:
-            ever+=1
-            if ever==3:
+        n_or_rever=1
+        counter=0
+        for rs in s:
+            
+            if n_or_ever==1:
+                if counter==0:
+                    a.append(list())
+                a.append(rs)
+                counter+=1
+                if counter==numRows:
+                    n_or_rever=0
+                    counter=0
+            else:
+                if counter==0:
+                    b.append(list())
                 b.append(rs)
-                ever=-1
-            if ever==0:
-                a.append(list())
-            a.append(rs)
-            ever+=1
+                counter+=1
+                if counter>=numRows-2:
+                    n_orrever=0
+                    counter=0
         result=[]
         for rs in range(len(a)):
             result.append(a[rs][0])
@@ -29,4 +43,3 @@ class Solution:
             else:
                 result.append(b[pb])
                 is_a=1
-                
